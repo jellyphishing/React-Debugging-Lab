18ac157fc2ff0fe246b42e4fb3bbe8f3a4cd750b
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-06 14:04:20 -0700

    refactored 2 components for App and BookTable so books can be accessed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 // generateBookData is used to simulate source data.
 // debug the "books" state variable to identify what that data looks like.
 // **DO NOT USE generateBookData ANYWHERE ELSE IN THIS LAB!**
-import { generateBookData } from './data/bookData';
+import { generateBookData } from "./data/bookData";
+import BookTable from "./components/BookTable/BookTable";
 
 const App = () => {
-	const [books, setBooks] = useState();
+  const [books, setBooks] = useState();
+  //console.log(books);
 
-	useEffect(() => {
-		setBooks(generateBookData()); // **DO NOT EDIT THIS LINE!**
-	});
+  useEffect(() => {
+    setBooks(generateBookData()); // **DO NOT EDIT THIS LINE!**
+  }, []);
 
-	return (
-		<div>
-			<h1>Book Depot</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Book Depot</h1>
+      <BookTable books={books} />
+    </div>
+  );
 };
 
 export default App;
